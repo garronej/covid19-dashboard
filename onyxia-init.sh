20f6bec7e220068cd985f44b4045167a5afa154f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2022-06-14 21:55:38 +0200

    Update onyxia-init.sh

--- onyxia-init.sh
+++ onyxia-init.sh
@@ -2,7 +2,7 @@ curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g yarn
 
-cd /home/coder/work/covid19-dashboard
-yarn
+# cd /home/coder/work/covid19-dashboard
+# yarn
 
 docker login -u $DOCKERHUB_USERNAME
