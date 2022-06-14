8c204c9f50f42172055af84305cc0d71862c1b29
Joseph Garrone <joseph.garrone.gj@gmail.com>
2022-06-14 21:35:38 +0200

    Create onyxia-init.sh

---
+++ onyxia-init.sh
@@ -0,0 +1,11 @@
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g yarn
+code-server --install-extension vscodevim.vim
+code-server --install-extension vscode-icons-team.vscode-icons
+
+git clone https://github.com/garronej/working_environnement && cd working_environnement && sudo ./apply.sh && source ~/.bashrc
+
+
+cd /home/coder/work/covid19-dashboard
+yarn
